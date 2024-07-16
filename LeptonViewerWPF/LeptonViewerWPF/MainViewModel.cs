4fc031d1c2ed7d32a2c2675b881a4e204a91e257
Kazuki Hosone <k-hosone@ray.co.jp>
2024-07-16 19:45:12 +0900

    ベース部分の構築
    ◆ dll のインポート
    ◆ UI・VIewModel の構築

--- LeptonViewerWPF/LeptonViewerWPF/MainViewModel.cs
+++ LeptonViewerWPF/LeptonViewerWPF/MainViewModel.cs
@@ -1,10 +1,87 @@
 ﻿using System.ComponentModel;
+using System.Windows.Media.Imaging;
 
 namespace LeptonViewerWPF
 {
     public class MainViewModel : INotifyPropertyChanged
     {
         #region Binding Variables
+        private float _minTemp = 22.0f;
+        public float MinTemp
+        {
+            get => _minTemp;
+            set
+            {
+                _minTemp = value;
+                RaisePropertyChanged(nameof(MinTemp));
+            }
+        }
+        private float _maxTemp = 40.0f;
+        public float MaxTemp
+        {
+            get => _maxTemp;
+            set
+            {
+                _maxTemp = value;
+                RaisePropertyChanged(nameof(MaxTemp));
+            }
+        }
+
+        private BitmapSource _leptonImage1;
+        public BitmapSource LeptonImage1
+        {
+            get => _leptonImage1;
+            set
+            {
+                _leptonImage1 = value;
+                RaisePropertyChanged(nameof(LeptonImage1));
+            }
+        }
+        private BitmapSource _leptonImage2;
+        public BitmapSource LeptonImage2
+        {
+            get => _leptonImage2;
+            set
+            {
+                _leptonImage2 = value;
+                RaisePropertyChanged(nameof(LeptonImage2));
+            }
+        }
+        private BitmapSource _leptonImage3;
+        public BitmapSource LeptonImage3
+        {
+            get => _leptonImage3;
+            set
+            {
+                _leptonImage3 = value;
+                RaisePropertyChanged(nameof(LeptonImage3));
+            }
+        }
+
+        private string _minMaxTemp = "Max Temp: 0.00°C, Min Temp: 0.00°C";
+        public string MinMaxTemp
+        {
+            get => _minMaxTemp;
+            set
+            {
+                _minMaxTemp = value;
+                RaisePropertyChanged(nameof(MinMaxTemp));
+            }
+        }
+
+        private string _mouseCursorTemp = "";
+        public string MouseCursorTemp
+        {
+            get
+            {
+                return _mouseCursorTemp;
+            }
+            set
+            {
+                _mouseCursorTemp = value;
+                RaisePropertyChanged(nameof(MouseCursorTemp));
+            }
+        }
         #endregion
 
         #region Commands
